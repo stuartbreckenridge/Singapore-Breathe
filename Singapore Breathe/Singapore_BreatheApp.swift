b140bcd04b54f6109cdb83c8f1b54ea3fca9a57e
Stuart Breckenridge <stuartb@hey.com>
2020-12-26 17:03:51 +0800

    Initial Commit

---
+++ Singapore Breathe/Singapore_BreatheApp.swift
@@ -0,0 +1,17 @@
+//
+//  Singapore_BreatheApp.swift
+//  Singapore Breathe
+//
+//  Created by Stuart Breckenridge on 26/12/2020.
+//
+
+import SwiftUI
+
+@main
+struct Singapore_BreatheApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
