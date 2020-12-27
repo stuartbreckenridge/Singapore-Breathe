f7cc2f96c6932bda10e5fffa68edeeea52287a35
Stuart Breckenridge <stuartb@hey.com>
2020-12-27 10:04:31 +0800

    Removes UI tests

--- Singapore BreatheTests/Singapore_BreatheTests.swift
+++ Singapore BreatheTests/Singapore_BreatheTests.swift
@@ -88,11 +88,4 @@ class Singapore_BreatheTests: XCTestCase {
         wait(for: [expectation], timeout: 5)
     }
 
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
-    }
-
 }
